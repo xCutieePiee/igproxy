fa3e26a9ff78be19917a7ad2113ca597624e8b96
CutiePie <51923365+xCutieePiee@users.noreply.github.com>
2022-07-17 19:53:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = new Koa();
 // app.set('trust proxy', true)
 
 // app.set('port', (process.env.PORT || 5000))
-app.use(proxy('https://www.instagram.com', {
+app.use(proxy('https://www.discord.com/app', {
   headers: {
     "set-cookie": ""
   }
