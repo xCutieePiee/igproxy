03e2dd921fb14d6246e808cb0fd757f3e2ac70ca
CutiePie <51923365+xCutieePiee@users.noreply.github.com>
2022-07-17 19:45:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ var app = new Koa();
 
 // app.set('port', (process.env.PORT || 5000))
 app.use(proxy('https://www.instagram.com', {
-  headers: {
-    "x-ig-app-id": '936619743392459',
-    "cookie": "csrftoken=ZXFvn643Os6i5hRHI7UN3evkfstEg6NQ; ds_user_id=17804375702; sessionid=17804375702%3AwQFMY6GSHyc7ph%3A10%3AAYeNMKE_K5hx7PIpb6RlIPVqeSOodNNudPs2nuHaqw;"
-  }
+  strippedHeaders: [
+    'set-cookie'
+  ]
 }))
 
 app.listen(process.env.PORT || 5000, function() {
