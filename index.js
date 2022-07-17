233fa1db9430b8891dd676c3076fc316321d604c
CutiePie <51923365+xCutieePiee@users.noreply.github.com>
2022-07-17 20:02:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,23 +10,6 @@ var app = new Koa();
 // app.set('port', (process.env.PORT || 5000))
 app.use(proxy('https://www.discord.com/channels/@me', {
   headers: {
-    "accept": "*/*",
-    "accept-language": "en-US",
-    "authorization": "NDk4MDk5MTU4NTI0MzYyNzY0.GF3k-f.ukxj3vg77vw5BkQDa8_ewMQORlPRNIc1u4pf1c",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "sec-gpc": "1",
-    "x-debug-options": "bugReporterEnabled",
-    "x-discord-locale": "en-US",
-    "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImZyLUZSIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgNi4xOyBXaW42NDsgeDY0KSBBcHBsZVdlYktpdC81MzcuMzYgKEtIVE1MLCBsaWtlIEdlY2tvKSBDaHJvbWUvMTAzLjAuNTA2MC4xMTQgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjEwMy4wLjUwNjAuMTE0Iiwib3NfdmVyc2lvbiI6IjciLCJyZWZlcnJlciI6Imh0dHBzOi8vcHJvYm90LmlvLyIsInJlZmVycmluZ19kb21haW4iOiJwcm9ib3QuaW8iLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTM3MDk1LCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=="
-  },
-  "referrer": "https://discord.com/channels/@me",
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": null,
-  "method": "GET",
-  "mode": "cors",
-  "credentials": "include"
   }
 }))
 
