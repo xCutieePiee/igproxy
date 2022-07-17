ad08930b3311fcd2c440cdaa243b0be2ebbf1b60
CutiePie <51923365+xCutieePiee@users.noreply.github.com>
2022-07-17 19:13:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express')
-var proxy = require('express-http-proxy');
+var proxy = require('koa-better-http-proxy');
 var app = express()
 
-app.set('trust proxy', true);
+app.set('trust proxy', true)
 
 app.set('port', (process.env.PORT || 5000))
-app.use(proxy('https://www.youtube.com'));
+app.use(proxy('https://www.youtube.com', {
+  preserveReqSession: true
+}))
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log('Bot online!')
 })
